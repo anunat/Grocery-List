f514b7482b8d05c55c9af839e9af4014d2623fab
anunat <anusha.hemanth@gmail.com>
2015-12-10 20:03:36 -0600

    12_10_3

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -21,7 +21,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # end
 
   def default_url
-    'default.jpg'
+    'assets/default.jpg'
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
